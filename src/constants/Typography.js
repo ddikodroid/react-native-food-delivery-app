48e4704ea9c804d155171a8f839d787439bfc7e8
ddikodroid <ddikodroid@gmail.com>
2021-04-13 22:47:11 +0700

    add typography

---
+++ src/constants/Typography.js
@@ -0,0 +1,23 @@
+export const FONT = {
+  contentTitle: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 18,
+    lineHeight: 20,
+  },
+  contentText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+    lineHeight: 22,
+  },
+  section: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: 22,
+    lineHeight: 24,
+    fontWeight: '900',
+  },
+  price: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: 22,
+    lineHeight: 28,
+  },
+};
