287665edb7765852f619abb9abe58ebddcebf00d
ddikodroid <ddikodroid@gmail.com>
2021-04-13 22:46:48 +0700

    add popular now content

---
+++ src/contents/PopularNow.js
@@ -0,0 +1,23 @@
+export const popularNow = [
+  {
+    id: 1,
+    title: 'Double Beef Burger',
+    subtitle: 'Extra yummy beef🥩',
+    price: 12.9,
+    image: require('../assets/images/double-beef-burger.png'),
+  },
+  {
+    id: 2,
+    title: 'Sandwich Burger',
+    subtitle: 'Why not both?🤤',
+    price: 11.9,
+    image: require('../assets/images/sandwich-burger.png'),
+  },
+  {
+    id: 3,
+    title: 'Bacon Burger',
+    subtitle: 'Best bacon in the town🥓',
+    price: 10.9,
+    mage: require('../assets/images/bacon-burger.png'),
+  },
+];
