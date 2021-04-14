673d2c7c62eccababb9bc29d66e40636c772b6b8
ddikodroid <ddikodroid@gmail.com>
2021-04-14 11:31:35 +0700

    add category content

---
+++ src/contents/Categories.js
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    icon: '🍔',
+    name: 'Burger',
+  },
+  {
+    icon: '🍕',
+    name: 'Pizza',
+  },
+  {
+    icon: '🥪',
+    name: 'Sandwich',
+  },
+  {
+    icon: '🌭',
+    name: 'Hot Dog',
+  },
+];
