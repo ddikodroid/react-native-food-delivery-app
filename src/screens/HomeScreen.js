be887c42e3c746321db0c973dea0423db7ddf06b
ddikodroid <ddikodroid@gmail.com>
2021-04-14 11:32:26 +0700

    add home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import Header from '../components/Header';
+import {COLOR} from '../constants/Color';
+import Banner from '../components/Banner';
+import Categories from '../components/Categories';
+import PopularNow from '../components/PopularNow';
+import {home} from '../contents/Home';
 
 const HomeScreen = () => {
+  const renderHome = ({item}) => {
+    switch (item.content) {
+      case 'banner':
+        return <Banner />;
+      case 'categories':
+        return <Categories />;
+      case 'popular':
+        return <PopularNow />;
+    }
+  };
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <FlatList
+        data={home}
+        renderItem={renderHome}
+        showsVerticalScrollIndicator={false}
+      />
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: COLOR.white,
+  },
+});
