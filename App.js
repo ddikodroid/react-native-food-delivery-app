ebc375a6439652c805a17728bdf817ade0abb6fb
ddikodroid <ddikodroid@gmail.com>
2021-06-01 12:33:12 +0700

    ignore all warning logs

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigations/AppNavigator';
+
+LogBox.ignoreAllLogs();
 
 const App = () => {
   return (
